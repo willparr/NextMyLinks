e01012ac1b9899c31dcf4dac54c3ff6d91294346
Will Parr <52459758+willparr@users.noreply.github.com>
2021-09-22 20:38:15 -0500

    Initialize auth-supabase

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+module.exports = {
+  purge: [
+    './src/lib/**/*.{jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Varela Round', ...fontFamily.sans],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
